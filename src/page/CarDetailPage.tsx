7de4bddddd153ae2c8d2e489c2ba3154476d3f0b
JamesVictor-o <victorjames40v@gmail.com>
2025-03-22 13:27:16 +0100

    ...

--- src/page/CarDetailPage.tsx
+++ src/page/CarDetailPage.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams, Link, useNavigate } from "react-router-dom";
+import { useParams, Link} from "react-router-dom";
 import { publicClient } from "./../utils/publicClient";
 import {formatEther } from "ethers";
 import { useAccount, useWriteContract } from "wagmi";
